378abe2feb85bc587a77629f1d2c2ddebc1c9ded
Victor Franco <victor.oliveirafranco@gmail.com>
2025-02-17 04:03:51 -0300

    fix(deps): update versions for Next.js compatibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules } from "@eslint/compat";
+import nextPlugin from "@next/eslint-plugin-next";
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +12,25 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default fixupConfigRules([
+  ...compat.extends("next/core-web-vitals"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      "@next/next": nextPlugin,
+      'react-hooks': reactHooksPlugin
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules
+    }
+  },
+  {
+    ignores: [
+      "**/node_modules/**",
+      ".next/**",
+      "public/**",
+      "next-env.d.ts"
+    ]
+  }
+]);
